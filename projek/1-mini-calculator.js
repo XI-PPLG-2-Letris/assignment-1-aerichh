f0153eaaf49dc09ab34efc2157a2915024398d45
aerichh <ecaa685@gmail.com>
2025-10-06 11:56:11 +0700

    sponsbob tugas mini kalkulator

---
+++ projek/1-mini-calculator.js
@@ -0,0 +1,12 @@
+let a = 20;
+let b = 10;
+
+const kalkulator = (a,b) => {
+    console.log(`Hasil dari penjumalahan ${a} + ${b} = ${a + b}`);
+    console.log(`Hasil dari pengurangan ${a} - ${b} = ${a - b}`);
+    console.log(`Hasil dari perkalian ${a} * ${b} = ${a * b}`);
+    console.log(`Hasil dari pembagian ${a} / ${b} = ${a / b}`);
+    console.log(`Hasil dari modulus ${a} % ${b} = ${a % b}`);
+    console.log(`Hasil dari perpangkatan ${a} ** ${b} = ${a ** b}`);
+}
+kalkulator(a,b);
